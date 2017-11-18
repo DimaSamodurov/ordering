1c42eff5d5b03d702a84e0bd87d4e4c8d0e67fbb
Dmytro Samodurov <dimasamodurov@gmail.com>
2017-11-18 02:32:31 +0200

    Add spec for user registration.

---
+++ spec/features/welcome/registration_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User registration', type: :system do
+  context 'with data entered correctly' do
+    scenario 'successful registration' do
+      visit root_path
+
+      click_on 'Register'
+      fill_in 'Email', with: 'testuser@sample.net'
+      fill_in 'Password', with: '123456'
+      fill_in 'Password confirmation', with: '123456'
+      click_on 'Sign up'
+
+      expect(page).to have_content 'You have signed up successfully. '
+    end
+  end
+end
