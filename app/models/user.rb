ef36957d8d067796dbaca9705d971b5baa3f69b1
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2017-12-06 16:06:17 +0200

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :first_name, :last_name, :email, :password, :remember_me
+  attr_accessor :first_name, :last_name, :role, :email, :password, :remember_me
 
   def name
-    "#{first_name} #{last_name}"
+    "#{first_name} #{last_name} #{role}"
   end
 end
