c174fd1483d7cbae47319748ece98357f939c0b7
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2017-12-16 11:17:29 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy], defaults: { format: 'js' }
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
 
   ActiveAdmin.routes(self)
 
-  resources :orders
+  #resources :orders
 
   root to: 'welcome#index'
 end
