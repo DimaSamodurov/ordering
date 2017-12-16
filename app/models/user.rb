bd09632d2e7aaea8c791671f99c447aa6b92d544
Dmytro Samodurov <dimasamodurov@gmail.com>
2017-12-16 20:56:57 +0200

    Display  List of User orders on the Home page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :first_name, :last_name, :role, :email, :password, :remember_me
+  has_many :orders
 
   def name
     "#{first_name} #{last_name}"
