2eec3cd7928c86586a6c6000317dfa7ebb05f7fd
Dmytro Samodurov <dimasamodurov@gmail.com>
2018-02-01 01:17:49 +0200

    Fix tests.

--- spec/features/orders/create_order_spec.rb
+++ spec/features/orders/create_order_spec.rb
@@ -2,25 +2,22 @@ require 'rails_helper'
 
 feature 'Create and order', type: :system do
   context 'with data entered correctly' do
-    let(:user) { user = User.create(email: 'user@sample.net', password: '123456') }
+    let(:user) { create :user }
 
     before do
       sign_in user
     end
 
-    scenario 'order is created successfully' do
+    scenario 'enter a new order' do
+      create :product, name: 'abrakadabra'
+
       visit root_path
 
       expect(page).to have_text('Ваші замовлення')
 
       click_on 'Нове замовлення'
-      fill_in 'Note', with: 'abrakadabra'
-      click_on 'Create Order'
 
-      expect(page).to have_content('Order was successfully created.')
       expect(page).to have_content('abrakadabra')
-
-      expect(Order.last.note).to eql 'abrakadabra'
     end
   end
 end
--- spec/features/welcome/registration_spec.rb
+++ spec/features/welcome/registration_spec.rb
@@ -5,13 +5,13 @@ feature 'User registration', type: :system do
     scenario 'successful registration' do
       visit root_path
 
-      click_on 'Register'
+      click_on 'Зареєструватись'
       fill_in 'Email', with: 'testuser@sample.net'
       fill_in 'Password', with: '123456'
       fill_in 'Password confirmation', with: '123456'
       click_on 'Sign up'
 
-      within '.orders-section' do
+      within 'main' do
         expect(page).to have_content 'Ваші замовлення'
       end
     end
