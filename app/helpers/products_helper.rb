0f7654c0cd9c3f13b620edf0bf4cb6eba0deae20
Dmytro Samodurov <dimasamodurov@gmail.com>
2018-01-31 23:51:44 +0200

    Display ProductPrices as a diagram.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+
+  def product_prices(name)
+    head = [
+      ['Date', 'Price'],
+    ]
+
+    prices = ProductPrice.for(name).map{|product| [product.effective_date.iso8601, product.price.to_f] }
+
+    head + prices
+  end
 end
