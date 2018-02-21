453b348ed99343cb78a2440f2de7442dce5d3a21
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-02-21 12:35:50 +0200

    active admin table for changing price

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -2,12 +2,27 @@ ActiveAdmin.register Product do
 
   permit_params :name, :price
 
+
   index do
     column :name
     column :price
     column :price_list do |product|
       Hash[product.price_list.map { |price| [price.effective_date.strftime('%FT%R'), price.price] }]
+
     end
-    actions
+
+    
+      column 'table product' do |product|
+        panel "Product prices" do
+          table_for ProductPrice.for(product.name).each do |pr|
+            column(:price)    {|pr| pr.price }
+            column(:effective_date)    { |pr| link_to(pr.effective_date, admin_product_price_path(pr)) }
+          end
+        end
+      end
+
+
+
+      actions
+    end
   end
-end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
 
   def charts
-    render layout: nil
+    @product = Product.find_by(params[:name]) 
+     render layout: nil
   end
+
 end
